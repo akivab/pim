98553b61e178d69852df7e17396b145b91ef6d6f
Akiva Bamberger <akiva.bamberger@gmail.com>
2011-05-15 17:05:04 -0400

    cleaned up js

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 var jQueryScriptOutputted = false;
-function directions(){
-    $(".body").load("directions.html");
+function loadpage(path){
+    try{
+	$(".body").load(path+".html");
+    }
+    except (err){
+	$(".body").load("main.html");
+    }
 }
 
-function instructors(){
-    $(".body").load("instructors.html");
-}
-
-function main(){
-    $(".body").load("main.html");
-}
-
-function contact(){
-    $(".body").load("contact.html");
-}
 
 function initJQuery() {
     //if the jQuery object isn't available
@@ -31,31 +25,7 @@ function initJQuery() {
         $(function() {
 		var paths = window.location.href.split("/");
 		var path=paths[paths.length-1];
-		if(path=="directions"){
-		    directions();
-		}
-		else if(path=="instructors"){
-		    instructors();
-		}
-		else if(path=="contact"){
-		    contact();
-		}
-		else{
-		    main();
-		}
-		$("#head").click(function(){ 
-			main(); 
-		    });
-		$("#directions").click(function(){
-			directions(); 
-		    });
-		$("#contact").click(function(){
-			contact(); 
-		    });
-		$("#instructors").click(function(){
-			instructors(); 
-		    });
-		
+		loadpage(path);
 	    });
     }
     
