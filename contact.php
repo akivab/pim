3d879e5a27eb3ee52a2c854a0839d661d8e81183
Akiva Bamberger <akiva.bamberger@gmail.com>
2011-05-24 02:05:31 -0400

    New Design

---
+++ contact.php
@@ -0,0 +1,13 @@
+      <!-- start of about -->
+      <div class="main-top">
+        <div class="body">
+          <div class="main_img">
+            <img src="/dance-studio-new/images/Mail.png" />
+          </div>
+          <div class="body-text">
+            <h1>Drop Us a Line</h1>
+            <p>Feel free to contact us at <a href='mailto:contact@palmyrainmotion.com'>contact@palmyrainmotion.com</a> or by phone at (315) 332 1088</p>
+	    </p>
+          </div>
+        </div>
+      </div>
---
+++ schedules.php
@@ -0,0 +1,14 @@
+      <!-- start of directions -->
+      <div class="schedule-top">
+        <div class="body">
+	  <div class="detailed-text">
+	    <h1>Schedule</h1>
+	    <div id="schedule">
+	    <iframe src="http://www.google.com/calendar/embed?src=palmyrainmotion.com_9ncpd2ps8h43k987m6qbu79620%40group.calendar.google.com&ctz=America/New_York" style="border: 0" width="500" height="300" frameborder="0" scrolling="no"></iframe>
+	    <p>Follow our Google calendar for current events.</p>
+	    <p>Check out our instructors <a href="/dance-studio-new/?p=instructors">here</a></p>
+	    </div>
+	  </div>
+	  </div>
+        </div>
+      </div>
