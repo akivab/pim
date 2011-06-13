02caaf1ee7520266191bf2ca247d10b14a7ffaed
Akiva Bamberger <akiva.bamberger@gmail.com>
2011-06-13 22:39:13 +0000

    made final changes

--- contact.php
+++ contact.php
@@ -2,11 +2,11 @@
       <div class="main-top">
         <div class="body">
           <div class="main_img">
-            <img src="/dance-studio-new/images/Mail.png" />
+            <img src="/images/Mail.png" />
           </div>
           <div class="body-text">
             <h1>Drop Us a Line</h1>
-            <p>Feel free to contact us at <a href='mailto:contact@palmyrainmotion.com'>contact@palmyrainmotion.com</a> or by phone at (315) 332 1088</p>
+            <p>Feel free to contact us at <a href='mailto:contact@palmyrainmotion.com'>contact@palmyrainmotion.com</a> or by phone at (315) 333 0623</p>
 	    </p>
           </div>
         </div>
--- index.php
+++ index.php
@@ -1,13 +1,17 @@
+<!doctype html>
 <html>
   <head>
+    <title>Palmyra in Motion</title>
+    <meta name="description" content="A fitness center for Palmyra, coming Summer 2011." />
+    <meta name="keywords" content="fitness, palmyra, rochester, zumba, ballet, dancing, dance studio" />
     <link href='http://fonts.googleapis.com/css?family=The+Girl+Next+Door' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="/dance-studio-new/css/reset.css" />
-    <link rel="stylesheet" href="/dance-studio-new/css/style.css" />
+    <link rel="stylesheet" href="/css/reset.css" />
+    <link rel="stylesheet" href="/css/style.css" />
   </head>
   <body>
     <div class="header">
       <div class="body">
-	<a href="/dance-studio-new"><div id="logo">palmyra<br />in motion</div></a>
+	<a href="/"><div id="logo">palmyra<br />in motion</div></a>
 	<!-- start of menu -->
 	<div class="menu">
 	  <ul>
@@ -16,7 +20,7 @@
 	       $words = array( "contact", "directions", "instructors", "schedules","home");
 	       for($i=0; $i<count($words); $i++){
 	          $j = $words[$i];
-		  echo "<li><a href='/dance-studio-new?p=$j'";
+		  echo "<li><a href='?p=$j'";
 		  if($_GET["p"] == $j){ 
 		    echo "class='selected'";
 	          }
@@ -42,10 +46,21 @@
 	<div>112 Fayette St, Palmyra NY</div>
 	<div>contact@palmyrainmotion.com</div>
 
-	<a target="_blank" href="https://www.facebook.com/pages/Palmyra-In-Motion/210715818959695"><img src='/dance-studio-new/images/Facebook.png' /></a>
+	<a target="_blank" href="https://www.facebook.com/pages/Palmyra-In-Motion/210715818959695"><img src='/images/Facebook.png' /></a>
 	<script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script>
-	<a href="http://twitter.com/share" target="_blank"><img src='/dance-studio-new/images/Twitter.png' /></a>
+	<a href="http://twitter.com/share" target="_blank"><img src='/images/Twitter.png' /></a>
       </div>
     </div>
+    <script type="text/javascript"> 
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-23367828-1']);
+      _gaq.push(['_trackPageview']);
+      
+      (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
   </body>
 </html>
