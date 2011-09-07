a16027845babe34134c0f323b5d0f30f95f78397
root <root@ip-10-244-33-6.ec2.internal>
2011-09-07 03:17:26 +0000

    edited about

--- about.php
+++ about.php
@@ -2,10 +2,10 @@
       <div class="directions-top">
         <div class="body">
 	  <div class="detailed-text">
-	    <h1>News</h1>
-   	    <h2>Find out why people are getting excited to dance!</h2>
-	    <p><a href="http://www.waynepost.com/feature/x386669556/Move-to-the-music-at-Palmrya-in-Motion#breaking_news">Link to article</a></p>
-	    <iframe width="100%" height="300px" src="http://www.waynepost.com/feature/x386669556/Move-to-the-music-at-Palmrya-in-Motion#breaking_news"> </iframe>
+	    <h1>About</h1>
+   	    <h2>Find out why people are getting excited to dance!</h2>	  
+	    <p><a target="_blank" href="http://www.waynepost.com/feature/x386669556/Move-to-the-music-at-Palmrya-in-Motion#breaking_news"><img width="300" src="images/wayne_logo.png" /></a></p>
+	<p>&nbsp;</p>
 	  </div>
 	</div>
       </div>
