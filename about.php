430e9ec9711b7f1c59e8fbf290bdbdf9be66be28
Akiva Bamberger <akiva.bamberger@gmail.com>
2011-09-06 23:04:09 -0400

    added zumba and about

---
+++ about.php
@@ -0,0 +1,12 @@
+<!-- start of directions -->
+      <div class="directions-top">
+        <div class="body">
+	  <div class="detailed-text">
+	    <h1>News</h1>
+   	    <h2>Find out why people are getting excited to dance!</h2>
+	    <p><a href="http://www.waynepost.com/feature/x386669556/Move-to-the-music-at-Palmrya-in-Motion#breaking_news">Link to article</a></p>
+	    <iframe width="100%" height="300px" src="http://www.waynepost.com/feature/x386669556/Move-to-the-music-at-Palmrya-in-Motion#breaking_news"> </iframe>
+	  </div>
+	</div>
+      </div>
+      
