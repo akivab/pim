f8e92a3c8a6ab3266cd43fc7d5022305f5ffe3e7
akiva <akiva.bamberger@gmail.com>
2011-11-02 10:01:37 -0400

    updated schedule page

--- schedules.php
+++ schedules.php
@@ -4,9 +4,7 @@
 	  <div class="detailed-text">
 	    <h1>Schedule</h1>
 	    <div id="schedule">
-	  <iframe src="https://www.google.com/calendar/embed?mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=palmyrainmotion.com_9ncpd2ps8h43k987m6qbu79620%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=America%2FNew_York" style=" border-width:0 " width="500" height="300" frameborder="0" scrolling="no"></iframe>
-	    <p>Follow our Google calendar for current events.</p>
-	    <p>Check out our instructors <a href="/?p=instructors">here</a></p>
+	  <iframe src="https://www.google.com/calendar/embed?mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=palmyrainmotion.com_9ncpd2ps8h43k987m6qbu79620%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=America%2FNew_York" style=" border-width:0 " width="900" height="400" frameborder="0" scrolling="no"></iframe>
 	    </div>
 	  </div>
 	  </div>
