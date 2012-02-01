0fb731ed44a603e4e0b40929313074d58ed5ea44
akiva <akiva.bamberger@gmail.com>
2012-02-01 03:27:59 -0500

    updated site

--- prices.php
+++ prices.php
@@ -3,7 +3,7 @@
         <div class="body">
 	  <div class="detailed-text">
 	    <h1>Prices</h1>
-		<h2>All classes with Leah are free.</h2>
+		<h2>Classes in February will be $2 for a single Zumba class, or $20 for unlimited Zumba in a month. Gentle yoga will be coming soon at $5 per class.</h2>
 		<p>&nbsp;</p>
 	  </div>
 	</div>
